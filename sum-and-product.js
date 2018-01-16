ceceaf9c9204a024305e71b122933bb0fa2df2da
Trinity Terry <trinityterry@Trinitys-MacBook-Air.local>
2018-01-15 20:54:05 -0600

    1/15/18

--- sum-and-product.js
+++ sum-and-product.js
@@ -4,21 +4,21 @@ Date Edited: 1/14/2018 */
 // You'll be writing a function that accepts an array of numbers and adds them up.
 
 // Create a variable with an array of numbers.
-var numbers = [4, 7, 8008, 11, 9, -1]
+var numbers = [4, 7, 8008, 11, 9, -1];
 // Create (declare) a function named addUpAllTheseNumbers.
 // The function should accept one argument: an array of numbers.
 // The function should have a for loop that loops over the array of numbers parameter and adds them to a running sum.
 function addUpAllTheseNumbers(numbers){
-    var sum = 0
+    var sum = 0;
     for(i = 0; i < numbers.length; i++){
-        sum = sum + numbers[i]
+        sum = sum + numbers[i];
     }
-    return sum 
+    return sum;
 }
 
 var mySum = addUpAllTheseNumbers(numbers);
 
-console.log(mySum)
+console.log(mySum);
 // The function should return the sum.
 // Call the function and pass in the array variable from step 1.
 // Save the results of the step above to a variable named mySum. 
